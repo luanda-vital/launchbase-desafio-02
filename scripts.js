b3bac4f07450c0190fd045781562ebb473d0fdaf
luanda-vital <luavital0@gmail.com>
2021-12-10 17:48:04 -0300

    ajuste do header para mobile e do layout do card

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+btnMobile.addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+});
+
+/* MODAL */
+
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
